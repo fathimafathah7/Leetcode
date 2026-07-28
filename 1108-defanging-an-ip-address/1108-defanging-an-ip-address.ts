6b1faa819fac55eb8484621312f534ce13505334
fathimafathah7 <fathimafathah421@gmail.com>
2026-07-28 21:10:51 +0530

    Time: 45 ms (43.94%), Space: 54.9 MB (66.67%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.ts
@@ -0,0 +1,14 @@
+function defangIPaddr(address: string): string {
+    let result = "";
+
+    for (let i = 0; i < address.length; i++) {
+        if (address[i] === ".") {
+            result += "[.]";
+        } else {
+            result += address[i];
+        }
+    }
+
+    return result;
+
+};
