2a3019f10d8d1f58e3abd2fbb22f171e69d5a387
fathimafathah7 <fathimafathah421@gmail.com>
2026-08-17 09:44:51 +0530

    Time: 2114 ms (6.1%), Space: 55 MB (82.93%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.ts
@@ -0,0 +1,9 @@
+function checkPerfectNumber(num: number): boolean {
+     let sum=0
+    for(let i=1;i<num;i++){
+        if(num%i==0){
+            sum=sum+i
+        }
+    }
+    return sum===num
+};
