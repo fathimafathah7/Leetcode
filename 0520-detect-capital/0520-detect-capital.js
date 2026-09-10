75ea27f2541cc211a810f456769c3b1e396fe6dd
fathimafathah7 <fathimafathah421@gmail.com>
2026-09-10 09:47:25 +0530

    Time: 0 ms (100%), Space: 53.8 MB (79.56%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    if(word===word.toUpperCase() ||
+    word===word.toLowerCase() ||
+    word===word[0].toUpperCase()+word.slice(1).toLowerCase()){
+        return true
+    }
+    else{
+        return false
+    }
+};
